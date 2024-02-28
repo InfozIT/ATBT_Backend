14ed36bea7bf2950c4fbf4c0865bcb2f19ff5c50
NirajNode24 <155453297+NirajNode24@users.noreply.github.com>
2024-02-28 14:30:15 +0530

    done

--- Controllers/toggle.js
+++ Controllers/toggle.js
@@ -7,21 +7,35 @@ const User = db.User
 const Add_toggle = async (req, res) => {
   try {
     const userId = req.params.id;
-    const user = await User.findByPk();
+
+    // Find the user by ID
+    const user = await User.findByPk(userId);
 
     // Toggle the User_status
     const newStatus = !user.userstatus;
-    console.log(newStatus)
-    const remarks = req.body.user_remarks_history;
+    const user_remarks_history = req.body.user_remarks_history;
+    const userJSONSTRINGFY = JSON.stringify(user_remarks_history)
 
     // Update the User_status in the database
-    const data = await User.update({ userstatus: newStatus, userremarkshistory: remarks }, { where: { id: userId } });
+    await User.update({ userstatus: newStatus, userremarkshistory: userJSONSTRINGFY }, { where: { id: userId } });
 
     // Fetch the updated user
     const updatedUser = await User.findByPk(userId);
-        res.status(200).json({
-      message: `User status updated successfully for ID: ${userId}`,data
+    
+    const a = updatedUser.userremarkshistory;
+
+    const jsonparsea = JSON.parse(a);
+      // const userremarkshistorys = {updatedUser}
+      // const userremarkshistory=JSON.parse(userremarkshistorys);
+
+      // const data = { ...updatedUser, userremarkshistory };
+    
+    res.status(200).json({
+      message: `User status updated successfully for ID: ${userId}`,
+      remark: jsonparsea,
+      user_status: updatedUser.userstatus
     });
+
   } catch (error) {
     console.error("Error updating user status:", error);
     res.status(500).json({ error: "Internal Server Error" });
