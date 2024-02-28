0675beb7642806f7e9d905bc9174fceb12e0ecbc
NirajNode24 <155453297+NirajNode24@users.noreply.github.com>
2024-02-28 14:58:19 +0530

    toggle

--- Controllers/toggle.js
+++ Controllers/toggle.js
@@ -13,27 +13,17 @@ const Add_toggle = async (req, res) => {
 
     // Toggle the User_status
     const newStatus = !user.userstatus;
-    const user_remarks_history = req.body.user_remarks_history;
-    const userJSONSTRINGFY = JSON.stringify(user_remarks_history)
+    const remarks = req.body.user_remarks_history;
+
 
     // Update the User_status in the database
-    await User.update({ userstatus: newStatus, userremarkshistory: userJSONSTRINGFY }, { where: { id: userId } });
+    await User.update({ userstatus: newStatus, userremarkshistory: remarks }, { where: { id: userId } });
 
-    // Fetch the updated user
-    const updatedUser = await User.findByPk(userId);
-    
-    const a = updatedUser.userremarkshistory;
 
-    const jsonparsea = JSON.parse(a);
-      // const userremarkshistorys = {updatedUser}
-      // const userremarkshistory=JSON.parse(userremarkshistorys);
-
-      // const data = { ...updatedUser, userremarkshistory };
     
     res.status(200).json({
       message: `User status updated successfully for ID: ${userId}`,
-      remark: jsonparsea,
-      user_status: updatedUser.userstatus
+ 
     });
 
   } catch (error) {
