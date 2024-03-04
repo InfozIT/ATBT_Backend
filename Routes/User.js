ae6202588d270ea8e13dc884cd3bc0243280aaad
NirajNode24 <155453297+NirajNode24@users.noreply.github.com>
2024-03-04 18:20:20 +0530

    done fitter

--- Routes/User.js
+++ Routes/User.js
@@ -5,7 +5,7 @@ const hasPermission = require('../middlewares/rolePermission');
 const upload = require('../utils/store');
 
 router.post('/create-user', hasPermission("entity", "canCreate"), upload.single('image'), User.Create_User)
-router.get('/list', hasPermission("user", "canRead"), User.List_User)
+router.post('/list', hasPermission("user", "canRead"), User.List_User)
 router.get('/list/:id', hasPermission("user", "canRead"), User.Get_User)
 router.put('/update/:id', hasPermission("user", "canUpdate"), upload.single('image'), User.Update_User)
 router.delete('/delete-user/:id', hasPermission("user", "canDelete"), User.Delete_User)
--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post('/upload', upload.single('image'), (req, res) => {
 
 
 app.get('/', (req, res) => {
-  res.send("mailer done")
+  res.send("filtter done")
 })
 // toggle 
 app.put('/toggle/:id', Toggle.Add_toggle)
