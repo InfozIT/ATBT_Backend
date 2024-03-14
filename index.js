c61dac563c5beff88c46547995733e843fd86ef0
NirajNode24 <155453297+NirajNode24@users.noreply.github.com>
2024-03-14 12:30:02 +0530

    r1

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post('/upload', upload.single('image'), (req, res) => {
 
 
 app.get('/', (req, res) => {
-  res.send("feat: Beta22444FDFF")
+  res.send("feat: Betar1")
 })
 // toggle 
 app.put('/toggle/:id', Toggle.Add_toggle)
