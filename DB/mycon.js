245f86df74eee103a2b75d0c6e453f1b8dfd57db
NirajNode24 <155453297+NirajNode24@users.noreply.github.com>
2024-03-29 10:46:23 +0530

    re

--- DB/mycon.js
+++ DB/mycon.js
@@ -1,24 +1,44 @@
-const mysql = require('mysql2');
-const config = require('./config')
+// const mysql = require('mysql2');
+// const config = require('./config')
 
-const mycon = mysql.createPool({
-  host: config.host,
-  user: config.user,
-  password: config.password,
-  database: config.database,
-  waitForConnections: true,
-  connectionLimit: 100,
-  queueLimit: 0
+// const mycon = mysql.createPool({
+//   host: config.host,
+//   user: config.user,
+//   password: config.password,
+//   database: config.database,
+//   waitForConnections: true,
+//   connectionLimit: 100,
+//   queueLimit: 0
+// });
+
+// // Explicitly connect to the database
+// mycon.getConnection((err, connection) => {
+//   if (err) {
+//     console.error('Error connecting to database: ' + err.stack);
+//     return;
+//   }
+//   console.log('Connected to database as id ' + connection.threadId);
+//   connection.release(); // Release the connection after obtaining it
+// });
+
+// module.exports = mycon.promise();
+
+const mysql = require('mysql2');
+
+const mycon = mysql.createConnection({
+  host: 'atbt.cdwoi4ikm5i7.ap-south-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'rootadmin',
+  database: 'JoinDb'
 });
 
-// Explicitly connect to the database
-mycon.getConnection((err, connection) => {
+// Connect to the database
+mycon.connect((err) => {
   if (err) {
     console.error('Error connecting to database: ' + err.stack);
     return;
   }
-  console.log('Connected to database as id ' + connection.threadId);
-  connection.release(); // Release the connection after obtaining it
+  console.log('Connected to database as id ' + mycon.threadId);
 });
 
-module.exports = mycon.promise();
+module.exports = mycon;
