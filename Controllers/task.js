3f6408e3031d031f009cdb505f12949d867ff840
NirajNode24 <155453297+NirajNode24@users.noreply.github.com>
2024-04-11 17:43:25 +0530

    revart--1712

--- Controllers/task.js
+++ Controllers/task.js
@@ -2,123 +2,48 @@ var db = require('../models/index');
 const Meet = db.Meeting;
 const mycon = require('../DB/mycon');
 
+const ListEntiyGroup = async (req, res) =>  {
+    const bmId = req.params.id;
+    const ids = [];
 
-// const ListEntiyGroup = async (req, res) => {
-//   const bmId = req.params.id;
-//   const ids = [];
-
-//   try {
-//     const Data = await Meet.findOne({ where: { id: bmId } });
-//     const result = await Meet.findAll({
-//       where: {
-//         id: bmId
-//       },
-//       attributes: ['members'] // Specify the column you want to retrieve
-//     });
-
-//     let members = result.map(meeting => meeting.dataValues.members).flat();
-//     ids.push(...members); // Spread the members array to push individual elements
-
-//     let EntID = Data.EntityId;
-
-//     mycon.query('SELECT * FROM Users WHERE EntityId = ?', EntID, async (err, result1) => {
-//       if (err) {
-//         console.error('Error retrieving data: ' + err.stack);
-//         res.status(500).send('Error retrieving data');
-//         return;
-//       }
-//       ids.push(...result1); // Spread the user IDs array to push individual elements
-
-//       // Removing duplicates from ids array
-//       const uniqIds = [...new Set(ids)];
-//       const ID = uniqIds.ids.map(obj => obj.id);
-//       console.log(ID)
-
-//       await Meet.update({ members: uniqIds }, {
-//         where: {
-//           id: bmId
-//         }
-//       });
-
-//       res.status(200).json({ ids: uniqIds }); // Sending unique ids array in the response
-//     });
-//   } catch (error) {
-//     console.error('Error: ' + error);
-//     res.status(500).send('Error processing request');
-//   }
-// };
-
-
-const ListEntiyGroup = async (req, res) => { // Changed function name to follow camelCase convention
-  const bmId = req.params.id;
-  const ids = [];
-
-  try {
-    const meetdata = await Meet.findOne({ where: { id: bmId } });
-    ids.push(...meetdata.members)
-    let EntID = (meetdata.EntityId)
-    
-    mycon.query('SELECT * FROM Users WHERE EntityId = ?', EntID, async (err, result1) => { // Passed EntID as an array
+    mycon.query('SELECT members, EntityId, TeamId FROM Meetings WHERE id = ?', bmId, (err, result) => {
       if (err) {
         console.error('Error retrieving data: ' + err.stack);
         res.status(500).send('Error retrieving data');
         return;
       }
-      // Extracting user IDs from result1 array
-      console.log(result1)
-      ids.push(...result1); // Spread the user IDs array to push individual elements
+      if (result.length === 0) {
+        res.status(404).send('Entity data not found');
+        return;
+      }
+      var EntID = (result[0].EntityId);
+      // Iterate over each member and push their id into ids array
+      for (let i = 0; i < result[0].members.length; i++) {
+        ids.push(result[0].members[i].id);
+      }
+      mycon.query('SELECT * FROM Users WHERE EntityId = ?', EntID, (err, result1) => {
+        if (err) {
+          console.error('Error retrieving data: ' + err.stack);
+          res.status(500).send('Error retrieving data');
+          return;
+        }
+        if (result1.length === 0) {
+          res.status(404).send('Entity data not found');
+          return;
+        }
+        for (let i = 0; i < result1.length; i++) {
+          if (!result1[i] ){
+            ids.push(result1[i].UserId);
 
-      // Removing duplicates from ids array
-      const uniqIds = [...new Set(ids)];
-
-
-      res.status(200).json({ ids: uniqIds }); // Sending unique ids array in the response
-
-
+          }
+        }
+        
+        const uniq = [...new Set(ids)];
+        
+        res.status(200).json({ ids: uniq }); // Sending ids array in the response
+      });
     });
-  } catch (error) {
-    console.error('Error: ' + error);
-    res.status(500).send('Error processing request');
-  }
 };
-
-
-// const ListTeamGroup = async (req, res) =>  {
-//   const bmId = req.params.id;
-//   const ids = [];
-//   mycon.query('SELECT members,TeamId FROM Meetings WHERE id = ?', bmId, (err, result) => {
-//     if (err) {
-//       console.error('Error retrieving data: ' + err.stack);
-//       res.status(500).send('Error retrieving data');
-//       return;
-//     }
-//     if (result.length === 0) {
-//       res.status(404).send('Entity data not found');
-//       return;
-//     }
-//     var EntID = (result[0].TeamId);
-//     // Iterate over each member and push their id into ids array
-//     for (let i = 0; i < result[0].members.length; i++) {
-//       ids.push(result[0].members[i].id);
-//     }
-//     mycon.query('SELECT * FROM UserTeam WHERE TeamId = ?', EntID, (err, result1) => {
-//       if (err) {
-//         console.error('Error retrieving data: ' + err.stack);
-//         res.status(500).send('Error retrieving data');
-//         return;
-//       }
-//       if (result1.length === 0) {
-//         res.status(404).send('Entity data not found');
-//         return;
-//       }
-//       for (let i = 0; i < result1.length; i++) {
-//         ids.push(result1[i].UserId);
-//       }
-//       const uniq = [...new Set(ids)];
-//       res.status(200).json({ ids: uniqids }); // Sending ids array in the response
-//     });
-//   });
-// };
 
 const ListTeamGroup = async (req, res) =>  {
   const bmId = req.params.id;
@@ -151,13 +76,13 @@ const ListTeamGroup = async (req, res) =>  {
       for (let i = 0; i < result1.length; i++) {
         ids.push(result1[i].UserId);
       }
-      // Filter out null values from the ids array
-      const filteredIds = ids.filter(id => id !== null);
-      const uniq = [...new Set(filteredIds)];
+      const uniq = [...new Set(ids)];
+      
       res.status(200).json({ ids: uniq }); // Sending ids array in the response
     });
   });
 };
+
 
 // CRUD for task module
 
