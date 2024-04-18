25d95105570875abf21a929f5d9c2a184ce35121
NirajNode24 <155453297+NirajNode24@users.noreply.github.com>
2024-04-18 15:59:08 +0530

    wip

--- Routes/Task.js
+++ Routes/Task.js
@@ -18,7 +18,7 @@ router.get('/team/group/:id',task.ListTeamGroup)
 router.post('/add/:id', hasPermission("task", "canCreate"), upload.single('image'), task.CreateTask)
 router.post('/list', hasPermission("task", "canRead"), task.ListTask)
 router.get('/list/:id', hasPermission("task", "canRead"),task.GetTask)
-router.put('/update/:id', hasPermission("task", "canUpdate"), upload.single('image'),task.UpdateTask)
+router.patch('/update/:id', hasPermission("task", "canUpdate"), upload.single('image'),task.UpdateTask)
 router.delete('/delete/:id', hasPermission("task", "canDelete"), task.DeleteTask)
 
 
