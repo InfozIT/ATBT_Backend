3565abfc8871f5468d1e0779a8e19e53c5b5e5e8
NirajNode24 <155453297+NirajNode24@users.noreply.github.com>
2024-05-07 17:34:53 +0530

    Update Task.js

--- Routes/Task.js
+++ Routes/Task.js
@@ -7,7 +7,7 @@ const hasPermission = require('../middlewares/rolePermission');
 // task 
 
 router.post('/add/:id', hasPermission("task", "canCreate"), upload.single('image'), task.CreateTask)
-router.get('/list/:id', hasPermission("task", "canRead"),task.GetTask)
+router.get('/list', hasPermission("task", "canRead"),task.GetTask)
 router.patch('/update/:id', hasPermission("task", "canUpdate"), upload.single('image'),task.UpdateTask)
 router.delete('/delete/:id', hasPermission("task", "canDelete"), task.DeleteTask)
 router.get('/listbyid/:id', hasPermission("task", "canRead"),task.GetTaskbyId)
