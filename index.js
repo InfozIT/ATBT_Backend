b2c40c2d7c86ac372fc32b7e552fe70c22cfec5a
balukapilguru <balakrishna.n@kapilguru.com>
2024-05-22 11:16:03 +0530

    some code added

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ app.get('/', (req, res) => {
           dynamic data sharing user & entity lcbi
           </li>
           <li>
-          task add comments ..
+          checking
           </li>
         </ul>
       </body>
