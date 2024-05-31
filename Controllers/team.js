78f4c78378ba31f93cd2851eb42126583c22f295
NirajNode24 <155453297+NirajNode24@users.noreply.github.com>
2024-05-31 15:42:18 +0530

    Fixed : Team status change to 201

--- Controllers/team.js
+++ Controllers/team.js
@@ -442,7 +442,7 @@ const UpdateTeam = async (req, res) => {
       await transporter.sendMail(mailData);
     }
 
-    res.status(200).json({ message: 'Team updated successfully' });
+    res.status(201).json({ message: 'Team updated successfully' });
   } catch (error) {
     console.error('Error updating Teams:', error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -684,7 +684,7 @@ const getTeamDataById = async (req, res) => {
         members: users,
       };
   
-      res.status(200).json(updatedTeam);
+      res.status(201).json(updatedTeam);
     } catch (error) {
       console.error('Error fetching meeting details:', error);
       res.status(500).json({ error: 'Internal Server Error' });
