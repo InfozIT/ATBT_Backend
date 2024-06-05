aa256c2354334f3b43af67821190b1c745e6b547
NirajNode24 <155453297+NirajNode24@users.noreply.github.com>
2024-06-05 14:37:44 +0530

    wip

--- Controllers/task.js
+++ Controllers/task.js
@@ -1049,9 +1049,14 @@ const UpdateTask = async (req, res) => {
         if (due.every(date => date != null) && dec.every(decision => decision != null)) {
           await transporter.sendMail(mailData);
           await db.Task.update(
-            { update_count: 1 },  // Set emailSent to true
-            { where: { id: req.params.id }, raw: true }  // Specify the task ID
+            { update_count: 1 },
+            {
+              where: {
+                id: req.params.id,
+              },
+            },
           );
+
 
         }
       }
